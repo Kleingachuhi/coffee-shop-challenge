d2c5677438265d6236641e49edb7b0a1d960a725
Klein <klein.gachuhi@student.moringaschool.com>
2025-05-18 14:04:13 +0300

    Initialized Customer

---
+++ customer.py
@@ -0,0 +1,14 @@
+class Customer:
+    def __init__(self, name):
+        self.name = name
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 1<= len(name) <= 15:
+            self._name = name
+        else:
+            raise Exception("The name must be a string with characters between 1 and 15.")
+        
+my_customer = Customer("Klein")
